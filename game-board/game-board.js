92da1866de0094ff0ef5f2ebaaa0617d5bdd6d03
Hannah Riley <riley.hannahm@gmail.com>
2020-02-03 14:29:04 -0800

    got our dynamic grid

--- game-board/game-board.js
+++ game-board/game-board.js
@@ -1,4 +1,34 @@
 // create grid, 12 by 8
+    // dynamically create 8 sections (rows)
+    // Fill with 12 cells (columns)
+    // Each cell has an id
+    // Each cell has a class
+    // Grid give an event listener
+
+const grid = document.getElementById('grid');
+
+export function renderRow() {
+
+    const maxColumns = 12;
+    const maxRows = 8;
+
+    for (let i = 0; i < maxRows; i++) {
+        const row = document.createElement('section');
+        row.id = `row-${i}`;
+        row.classList.add('row');
+
+        for (let j = 0; j < maxColumns; j++) {
+            const cell = document.createElement('div');
+            cell.id = `grid-${i}-${j}`;
+            cell.classList.add('cell');
+            row.appendChild(cell);
+        }
+        grid.appendChild(row);
+    }
+
+}  
+
+renderRow();
 
 // place starting river tiles ~8
 
@@ -14,3 +44,11 @@
 
 //end game button
 
+// 
+
+
+    const div = document.getElementById('grid-0-6');
+    div.style.opacity = 1;
+    div.style.background = 'url("../common/CastleCenter0.png")';
+    div.style.backgroundSize = 'cover';
+    // container.appendChild(div);
