d948d66931873eb6355ed901eefcffe8422a26be
Hannah Riley <riley.hannahm@gmail.com>
2020-02-03 15:02:18 -0800

    Import data, add random tile to grid

--- game-board/game-board.js
+++ game-board/game-board.js
@@ -1,3 +1,5 @@
+import { tiles } from '../data/tiles.js';
+
 // create grid, 12 by 8
     // dynamically create 8 sections (rows)
     // Fill with 12 cells (columns)
@@ -5,9 +7,8 @@
     // Each cell has a class
     // Grid give an event listener
 
-const grid = document.getElementById('grid');
 
-export function renderRow() {
+export function renderGrid(parent) {
 
     const maxColumns = 12;
     const maxRows = 8;
@@ -23,18 +24,28 @@ export function renderRow() {
             cell.classList.add('cell');
             row.appendChild(cell);
         }
-        grid.appendChild(row);
+        parent.appendChild(row);
     }
-
 }  
 
-renderRow();
+const grid = document.getElementById('grid');
+renderGrid(grid);
+
+grid.addEventListener('click', (e) => {
+    // TBD
+});
+
 
 // place starting river tiles ~8
 
 // import the tiles
 
 // create deck / get tile function 
+function getRandomTile() {
+    const randomTile = Math.ceil(Math.random() * Object.keys(tiles).length);
+    return tiles[randomTile];
+}
+
 
 // on click, get grid space id and place tile
 
@@ -46,9 +57,10 @@ renderRow();
 
 // 
 
-
-    const div = document.getElementById('grid-0-6');
-    div.style.opacity = 1;
-    div.style.background = 'url("../common/CastleCenter0.png")';
-    div.style.backgroundSize = 'cover';
-    // container.appendChild(div);
+const retrievedTile = getRandomTile();
+const div = document.getElementById('grid-0-6');
+div.style.opacity = 1;
+console.log(retrievedTile.image);
+div.style.backgroundImage = `url("../tiles/${retrievedTile.image}")`;
+div.style.backgroundSize = 'cover';
+// container.appendChild(div);
