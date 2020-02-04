a8077d44f1bc93a67e43cd80256841d47a4af411
Fiona Ochs <fiona.oaks@gmail.com>
2020-02-04 11:09:54 -0800

    log in css

---
+++ log-in/log-in.js
@@ -0,0 +1,18 @@
+import { saveUser } from './api.js';
+import makeUser from './make-user.js';
+
+//grab our form from home page so we can access on submit
+const userSignUp = document.getElementById('user-sign-up');
+//add event listener for when form is submitted
+userSignUp.addEventListener('submit', function(event) {
+    event.preventDefault();
+    //create new formData
+    const formData = new FormData(userSignUp);
+    //use form data to make user
+    const user = makeUser(formData);
+    //save user in local storage
+    saveUser(user);
+    //send to game-board page to start adventure
+    window.location = 'game-board';
+        
+});
