8f663c58e31348c6936811d0429396de80ace8ee
Fiona Ochs <fiona.oaks@gmail.com>
2020-02-04 11:41:37 -0800

    test 1

---
+++ test/functionstotest.js
@@ -0,0 +1,32 @@
+export function getPlacedTiles(gameState) {
+    let placedTilesArray = [];
+
+    //loop through all played tile ids in gameState
+    gameState.forEach(row => {
+        row.forEach(cell => {
+            if (cell) placedTilesArray.push(cell);
+        });
+    });
+    return placedTilesArray;
+}
+
+
+export function getUnplayedTiles() {
+    const placedTiles = getPlacedTiles();
+    let unplayedTiles = Object.keys(tiles).filter(id => {
+        return placedTiles.indexOf(Number(id)) < 0;
+    });
+}
+
+export function makeBlankGameState() {
+    
+    // Loop through maxRows and create rows
+    for (let i = 0; i < maxRows; i++) {
+        //make new array for every row in grid array
+        gameState.push(new Array());
+        //make null placeholder for each cell in grid
+        for (let j = 0; j < maxColumns; j++) {
+            gameState[i].push(null);
+        }
+    }
+}  
