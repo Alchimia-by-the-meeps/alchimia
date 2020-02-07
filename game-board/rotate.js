d579b867221b7323394153aa6a105062b2dd34c6
Nathan Martel <nathanmartel@gmail.com>
2020-02-06 19:08:00 -0800

    finishing touches

--- game-board/rotate.js
+++ game-board/rotate.js
@@ -1,35 +1,23 @@
-
 // event listener on both buttons
 // on click transform the image per button selection (left/right)
     // on rotation, need to update the sides [0, 1, 2, 3]
     // get the current rotation 
 
-const rightButton = document.getElementById('rotate-right');
-const leftButton = document.getElementById('rotate-left');
-const getPlayerTile = document.getElementById('player-tile');
-
-
 //import function to game-board (only call in gameboard)
 export function rotateTile(topDeckTile, direction) {
+    const getPlayerTile = document.getElementById('player-tile');
     const tileToRotate = topDeckTile;
     if (direction === 'right') {
         tileToRotate.sides.unshift(tileToRotate.sides.pop());
         tileToRotate.rotation = tileToRotate.rotation + 90;
         getPlayerTile.style.transform = 'rotate(' + tileToRotate.rotation + 'deg)';
+        getPlayerTile.style.transition = '.3s';
 
     } else if (direction === 'left') {
         tileToRotate.sides.push(tileToRotate.sides.shift());
         tileToRotate.rotation = tileToRotate.rotation - 90;
         getPlayerTile.style.transform = 'rotate(' + tileToRotate.rotation + 'deg)';
+        getPlayerTile.style.transition = '.3s';
     } 
     return tileToRotate;
 }
-
-// game board JS
-// rightButton.addEventListener('click', () => {
-//     topDeckTile = rotateTile(topDeckTile, 'right');
-// });
-
-// leftButton.addEventListener('click', () => {
-//     topDeckTile = rotateTile(topDeckTile, 'left');
-// });
